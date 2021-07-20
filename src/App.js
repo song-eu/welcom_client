9cb4a22fa331597c2109871f125ac2aec9a9fb26
song-eu <el9001@gmail.com>
2021-07-21 00:39:28 +0900

    add d3 chart

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Redirect, Link } from 'react-router-dom';
 import * as d3 from 'd3';
+import VisitOutpatient from './components/visitOutpatient';
 
 const App = () => {
     d3.selectAll('p').style("color", "blue")
@@ -10,6 +12,9 @@ const App = () => {
           Welcome dashboard with D3
         </p>
       </header>
+      <Router>  
+        <Route exact path="/" component = {VisitOutpatient}/>
+      </Router>
     </div>
   );
 }
